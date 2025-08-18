deb5c089a679f2f6d2e5f0457badf5d71af6e9a3
unknown <ishimbay-avon@yandex.ru>
2025-08-18 14:13:07 +0500

    #2

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import mongoose from 'mongoose';
+import { errors } from 'celebrate';
 import authRouter from './routes/auth';
 import userRouter from './routes/user';
 import { errorLogger, requestLogger } from './middlewares/logger';
@@ -9,19 +10,9 @@ import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
-app.use(requestLogger);
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use('/auth', authRouter);
-
-app.use('/user', userRouter);
-
-app.use(errorLogger);
-
-app.use(errorHandler);
 
 mongoose
   .connect(DB_ADDRESS)
@@ -31,6 +22,16 @@ mongoose
   .catch((err) => {
     console.error('Ошибка подключения:', err);
   });
+
+app.use(requestLogger);
+
+app.use('/auth', authRouter);
+
+app.use('/user', userRouter);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
--- src/middlewares/validatons.ts
+++ src/middlewares/validatons.ts
@@ -11,20 +11,25 @@ const objectIdValidator = (value: string, helpers: any) => {
 
 export const validateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).messages({
-      'string.min': 'Минимальная длина поля "name" - 2',
-      'string.max': 'Максимальная длина поля "name" - 30',
-    }),
+    fullname: Joi.string().min(2).max(30).required()
+      .messages({
+        'string.min': 'Минимальная длина поля "name" - 2',
+        'string.max': 'Максимальная длина поля "name" - 30',
+        'any.required': 'Поле "name" обязательно',
+      }),
     password: Joi.string().min(6).max(100).required()
       .messages({ 'string.empty': 'Поле "password" должно быть заполнено' }),
     email: Joi.string()
       .required()
       .email()
       .max(100)
-      .message('Поле "email" должно быть валидным email-адресом')
       .messages({
+        'string.email': 'Поле "email" должно быть валидным email-адресом',
         'string.empty': 'Поле "email" должно быть заполнено',
+        'any.required': 'Поле "email" обязательно',
       }),
+    birthdate: Joi.date().iso().optional(),
+    role: Joi.string().valid('user', 'admin').optional(),
   }),
 });
 
@@ -69,8 +74,12 @@ export const validateBlockUser = celebrate({
 
 export const validateObjId = celebrate({
   params: Joi.object().keys({
-    productId: Joi.string()
+    id: Joi.string()
       .custom(objectIdValidator)
-      .required(),
+      .required()
+      .messages({
+        'any.required': 'Параметр id обязателен',
+        'string.empty': 'Параметр id не может быть пустым',
+      }),
   }),
 });
